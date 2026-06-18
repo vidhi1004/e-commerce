9e74c786ebb0e377b4b145c309ff1e2e1907801f
vidhi1004 <vidhishrimal04@gmail.com>
2026-06-18 16:40:04 +0530

    order service change

--- backend/order-service/src/main.ts
+++ backend/order-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://guest:guest@localhost:5672'],
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
